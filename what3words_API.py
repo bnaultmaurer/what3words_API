8f05633742a2ed9ccb9a655af480b7bbf86ced1f
Benj <bnaultmaurer@gmail.com>
2020-05-20 23:11:06 -0500

    remove checks, add final comments

--- what3words_API.py
+++ what3words_API.py
@@ -3,14 +3,16 @@ import pandas as pd
 import numpy as np
 import csv
 
-
+##### define inputs #####
 # define API key
 api_key = "JBY0UH4P"
 
 input_file = 'mn_county_centroids.txt'
 output_file ='mn_county_centroids_out.csv'
+##### inputs complete #####
 
-## connect to public API
+
+# connect to public API
 geocoder = what3words.Geocoder(api_key)
 
 # read in set of coordinates
@@ -19,13 +21,10 @@ df['what3words'] = None
 
 # iterate through values
 for index, row in df.iterrows():
-    #lat_long = str(df['lat'][index]) + ", " + str(df['long'][index])
-    #print(lat_long)
 
     # convert coordinates to what3words
     res = geocoder.convert_to_3wa(what3words.Coordinates(df['lat'][index], df['long'][index]))
     df['what3words'][index] = res['words']
-    #print(res['words'], df['what3words'][index])
 
 # write dataframe to file
 df.to_csv(output_file)
