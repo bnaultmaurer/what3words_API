5ca5bc135b121342ae1cc5b1eb087003c32c4b8b
Benj <bnaultmaurer@gmail.com>
2020-05-20 23:03:44 -0500

    implement to write locations out to console

--- what3words_API.py
+++ what3words_API.py
@@ -7,19 +7,20 @@ import csv
 # define API key
 api_key = "JBY0UH4P"
 
+input_file = 'mn_county_centroids.txt'
+output_file ='mn_county_centroids_out.txt'
+
 ## connect to public API
 geocoder = what3words.Geocoder(api_key)
 
 # read in set of coordinates
-df = pd.read_csv('mn_county_centroids.txt', sep = '\t', skiprows = 1, names = ('stand', 'long', 'lat'))
+df = pd.read_csv(input_file, sep = '\t', skiprows = 1, names = ('stand', 'long', 'lat'))
 
 # iterate through values
 for index, row in df.head(5).iterrows():
-    lat_long = str(df['lat'][index]) + ", " + str(df['long'][index])
-    print(lat_long)
+    #lat_long = str(df['lat'][index]) + ", " + str(df['long'][index])
+    #print(lat_long)
 
-
-
-## convert coordinates to what3words
-#res = geocoder.convert_to_3wa(what3words.Coordinates(46.25, -93.25))
-#print(res['words'])
+    # convert coordinates to what3words
+    res = geocoder.convert_to_3wa(what3words.Coordinates(df['lat'][index], df['long'][index]))
+    print(res['words'])
